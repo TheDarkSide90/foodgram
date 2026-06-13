243da779c49937ea3b058783f647cc680a9607e3
Nikita Vasilev <nikitas2000@mail.ru>
2026-06-13 23:26:35 +0300

    Changed

--- backend/backend/foodgram_backend/settings.py
+++ backend/backend/foodgram_backend/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*o9-q242_e+pd^7^$f8o26b)8f(g5r--6l5&koq!)zswi6#0b_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://foodgramproject.mooo.com",
+    "http://foodgramproject.mooo.com",
 ]
 
 
