ae6805451c3b67003935cea8f03f0597d22a14c6
Nikita Vasilev <nikitas2000@mail.ru>
2026-06-17 03:01:44 +0300

    Changed

--- backend/backend/foodgram_backend/urls.py
+++ backend/backend/foodgram_backend/urls.py
@@ -6,5 +6,5 @@ from api.views import short_link_redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('s/<str:code>/', short_link_redirect)
+    path('s/<str:code>', short_link_redirect)
 ]
